28c20878b7f288ad8b692950c47a07b4c5397e56
hailee.miu <hailee.miu@gmail.com>
2023-10-07 00:00:20 +0000

    GET with url query

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/haileemiu/manage-life
 
 go 1.21.2
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.0.10 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+// TODO: try chi router nested - see github docs
 func main() {
-	fmt.Println("hello world")
+	r := chi.NewRouter()
+
+	r.Use(
+		middleware.RequestID,
+		middleware.RealIP,
+		middleware.Logger,
+		middleware.Recoverer,
+		middleware.Timeout(60*time.Second), // TODO: make config variable
+	)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		test := r.URL.Query().Get("test")
+		if test == "" {
+			test = "World"
+		}
+
+		fmt.Fprintf(w, "Hi %s", test)
+	})
+
+	http.ListenAndServe(":4000", r)
 }
