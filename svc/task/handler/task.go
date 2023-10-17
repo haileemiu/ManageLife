a004dcb241b14497777d7afeffad601a59157966
hailee.miu <hailee.miu@gmail.com>
2023-10-17 00:16:32 +0000

    update

--- svc/task/handler/task.go
+++ svc/task/handler/task.go
@@ -74,13 +74,45 @@ func (t Task) create(w http.ResponseWriter, r *http.Request) {
 }
 
 func (t Task) getByID(w http.ResponseWriter, r *http.Request) {
+	// TODO: both error handling
 	taskID, _ := strconv.Atoi(chi.URLParam(r, "id"))
 	task, _ := t.ent.Task.Query().Where(task.ID(taskID)).Only(r.Context())
 	json.NewEncoder(w).Encode(task)
 }
 
 func (t Task) update(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Not Implemented")
+	taskID, _ := strconv.Atoi(chi.URLParam(r, "id"))
+
+	req := model.TaskCreateRequest{}
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	if ok, errs := req.Validate(); !ok {
+		res.NewValidationErrorResponse(errs).Send(w)
+		return
+	}
+
+	task, _ := t.ent.Task.UpdateOneID(taskID).
+		SetTitle(req.Title).
+		SetNotes(req.Notes).
+		SetIsImportant(req.IsImportant).
+		SetIsTimeSenstive(req.IsImportant).
+		SetDueAt(req.DueAt).
+		SetRemindAt(req.RemindAt).
+		// TODO: update at
+		Save(r.Context())
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	// TODO: handle error
+	// TODO: set response encoding
+	// TODO: convert from ent model to task model
+	json.NewEncoder(w).Encode(task)
 }
 
 func (t Task) delete(w http.ResponseWriter, r *http.Request) {
